51c386926212f4719ebd6457e3b5b1810fa52c91
yinyongxian <yinyongxian@qq.com>
2021-06-29 10:09:32 +0800

    JsonConvert

---
+++ YYX.CATest/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YYX.CATest
+{
+    public class Person
+    {
+        private int age;
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public DateTime? ShippingDate { get; set; }
+    }
+}
--- YYX.CATest/Program.cs
+++ YYX.CATest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace YYX.CATest
 {
@@ -9,10 +10,27 @@ namespace YYX.CATest
     {
         static void Main(string[] args)
         {
-            var joinString = string.Join("abcd", "abcdefg");
-            Console.WriteLine(joinString);
+            Hello();
 
             Console.ReadKey();
+        }
+
+        private static void Hello()
+        {
+            Person person = new Person
+            {
+                Age = 18,
+                Name = "YYX",
+                ShippingDate = null,
+            };
+
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore
+
+            };
+            string serializeObject = JsonConvert.SerializeObject(person, jsonSerializerSettings);
         }
     }
 }
