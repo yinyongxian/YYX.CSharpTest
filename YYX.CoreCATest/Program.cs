5c2c4df1be72d4960d7f8485f21789ed23c729e0
yinyongxian <yinyongxian@qq.com>
2022-02-24 10:41:17 +0800

    0

--- YYX.CoreCATest/Program.cs
+++ YYX.CoreCATest/Program.cs
@@ -9,19 +9,20 @@ Console.WriteLine("Hello, World!");
 
 
 DataTable dataTable = new DataTable();
-dataTable.Columns.Add("A", typeof(decimal));
-dataTable.Columns.Add("B", typeof(decimal));
-dataTable.Columns.Add("C", typeof(decimal));
+dataTable.Columns.Add("A", typeof(DateTime));
+//dataTable.Columns.Add("B", typeof(decimal));
+//dataTable.Columns.Add("C", typeof(decimal));
 for (int i = 0; i < 5; i++)
 {
     DataRow dataRow = dataTable.NewRow();
-    dataRow["A"] = 1 * i;
-    dataRow["B"] = 2 * i;
-    dataRow["C"] = 3 * i;
+    dataRow["A"] = null;
+    //dataRow["B"] = 2 * i;
+    //dataRow["C"] = 3 * i;
     dataTable.Rows.Add(dataRow);
 }
 
 
-object compute = dataTable.Compute("Sum(B)", "");
+var filterExpression = string.Format("A >= '{0}'", DateTime.Now);
+DataRow[] dataRows = dataTable.Select(filterExpression);
 
 Console.ReadKey();
