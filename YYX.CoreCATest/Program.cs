ce2a6df812040d12f0c8f811c79ca7a00cf8a5ee
yinyongxian <yinyongxian@qq.com>
2022-02-25 12:33:22 +0800

    update

--- YYX.CoreCATest/Program.cs
+++ YYX.CoreCATest/Program.cs
@@ -8,21 +8,16 @@ using YYX.CoreCATest;
 Console.WriteLine("Hello, World!");
 
 
-DataTable dataTable = new DataTable();
-dataTable.Columns.Add("A", typeof(DateTime));
-//dataTable.Columns.Add("B", typeof(decimal));
-//dataTable.Columns.Add("C", typeof(decimal));
-for (int i = 0; i < 5; i++)
+string[] groupFunctions = Enum.GetNames(typeof(DayOfWeek));
+var query =
+    from groupFunction in groupFunctions
+    select groupFunction.ToString().ToUpper();
+
+DayOfWeek result;
+bool success = Enum.TryParse( "SUNDAY1", true, out result);
+if (success)
 {
-    DataRow dataRow = dataTable.NewRow();
-    dataRow["A"] = null;
-    //dataRow["B"] = 2 * i;
-    //dataRow["C"] = 3 * i;
-    dataTable.Rows.Add(dataRow);
+    
 }
-
-
-var filterExpression = string.Format("A >= '{0}'", DateTime.Now);
-DataRow[] dataRows = dataTable.Select(filterExpression);
 
 Console.ReadKey();
